7f18a5277be5022d4647e53fa28de2b9c3014a7a
마광휘 <mgh950714@gmail.com>
2020-04-19 00:44:33 +0900

    [컴포넌트 구현] molecules/Card 구현

---
+++ src/components/molecules/Card/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import classNames from 'classnames'
+
+import { IComponentProps } from '../../../models/common'
+import { CalculateBox } from '../../../utils'
+
+import styles from './style.module.scss'
+
+interface IProps extends IComponentProps {
+  radius?: [number, number?, number?, number?]
+  padding?: [number, number?, number?, number?]
+}
+
+const Card: React.FC<IProps> = ({ className, radius = [0], padding = [0] }) => {
+  const classProps = classNames(className, styles.default)
+  const styleProps = { borderRadius: CalculateBox(radius).toString(), padding: CalculateBox(padding).toString() }
+
+  return <div className={classProps} style={styleProps} />
+}
+
+export default Card
