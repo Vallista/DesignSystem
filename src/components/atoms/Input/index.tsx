f7d6d426911595a1f9fa72d172a9dc1595ee610a
마광휘 <mgh950714@gmail.com>
2020-04-19 00:28:50 +0900

    [컴포넌트 구현] atoms/Input 구현

---
+++ src/components/atoms/Input/index.tsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import classNames from 'classnames'
+
+import styles from 'style.module.scss'
+
+import { IComponentProps } from '../../../models/common'
+
+export enum InputType {
+  BUTTON = 'button',
+  CHECKBOX = 'checkbox',
+  DATE = 'date',
+  EMAIL = 'email',
+  FILE = 'file',
+  hidden = 'HIDDEN',
+  IMAGE = 'image',
+  NUMBER = 'number',
+  PASSWORD = 'password',
+  RADIO = 'radio',
+  SUBMIT = 'submit',
+  TEXT = 'text',
+  TEL = 'tel',
+  SEARCH = 'search',
+  TIME = 'time'
+}
+
+interface IProps extends IComponentProps {
+  className?: string
+  value: string | number
+  type?: InputType
+  onChange?(event: React.ChangeEvent<HTMLInputElement>): void
+}
+
+const Input: React.FC<IProps> = ({ className, value, type, isCapturing, onChange }) => {
+  const classProps = classNames(styles.default, className)
+  const changeEvent = isCapturing ? { onChangeCapture: onChange } : { onChange }
+
+  return <input className={classProps} type={type} value={value} {...changeEvent} />
+}
+
+export default Input
