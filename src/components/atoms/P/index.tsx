34d25fe4655a0119edabf3aafd1dec829b402af0
마광휘 <mgh950714@gmail.com>
2020-04-19 16:39:37 +0900

    [컴포넌트 수정] 줄이 길어질 때 줄임표 동작하는 기능 추가

--- src/components/atoms/P/index.tsx
+++ src/components/atoms/P/index.tsx
@@ -20,21 +20,57 @@ export enum WhiteSpace {
   PRE_WRAP = 'pre-wrap'
 }
 
+export enum TextAlign {
+  LEFT = 'left',
+  CENTER = 'center',
+  RIGHT = 'right'
+}
+
 interface IProps extends IComponentProps {
   children: React.ReactNode
   lineHeight?: number
   weight?: number
   color?: ColorType
-  margin?: [number, number?, number?, number?]
   wordBreak?: WordBreak
   whiteSpace?: WhiteSpace
+  size?: number
+  align?: TextAlign
+  ellipsis?: boolean
 }
 
-const P: React.FC<IProps> = ({ className, children, color = ColorPalette.Black.BLACK, weight = 300, lineHeight = 1, margin = [0], wordBreak = WordBreak.KEEP, whiteSpace = WhiteSpace.NORMAL }) => {
+const P: React.FC<IProps> = ({
+  className,
+  children,
+  color = ColorPalette.Black.BLACK,
+  weight = 300,
+  lineHeight = 1,
+  margin = [ 0 ],
+  wordBreak = WordBreak.KEEP,
+  whiteSpace = WhiteSpace.NORMAL,
+  size = 12,
+  align = TextAlign.LEFT,
+  ellipsis = false
+}) => {
   const classProps = classNames(styles.default, className)
-  const styleProps = { color, fontWeight: weight, lineHeight, margin: CalculateBox(margin), wordBreak, whiteSpace }
+  const styleProps = {
+    color,
+    fontWeight: weight,
+    lineHeight,
+    margin: CalculateBox(margin),
+    wordBreak,
+    whiteSpace: ellipsis ? WhiteSpace.NOWRAP : whiteSpace,
+    fontSize: size,
+    textAlign: align,
+    textOverflow: ellipsis ? 'ellipsis' : 'clip',
+    overflow: ellipsis ? 'hidden' : 'visible',
+    width: ellipsis ? 'calc(100% - 0px)' : '100%'
+  }
 
-  return <p className={classProps} style={styleProps}>{children}</p>
+  return (
+    <p className={classProps} style={styleProps}>
+      {children}
+    </p>
+  )
 }
 
 export default P
