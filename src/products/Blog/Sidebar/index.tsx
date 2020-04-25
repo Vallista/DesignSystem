a576eb4eecebcc47644b7bba6d5bdf5011b88496
마광휘 <mgh950714@gmail.com>
2020-04-25 22:57:44 +0900

    [프로덕트 구현 시작] Sidebar component 구현 2

--- src/products/Blog/Sidebar/index.tsx
+++ src/products/Blog/Sidebar/index.tsx
@@ -1,7 +1,39 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
-const Sidebar = () => {
-  return <div>123</div>
+import Block, { Direction } from 'components/molecules/Block'
+
+export interface ISidebarComponentProps {}
+
+export interface ISidebar {
+  name: string
+  component: React.FunctionComponent<ISidebarComponentProps>
+  default?: boolean
+}
+
+interface IProps {
+  head: React.ReactNode
+  value: string
+  config: ISidebar[]
+}
+
+const Sidebar: React.FC<IProps> = ({ head, value, config }) => {
+  const [ NowComponent, setNowComponent ] = useState<React.FunctionComponent<ISidebarComponentProps>>(
+    config.find((conf) => conf.name === value)!.component || config[0].component
+  )
+
+  useEffect(
+    () => {
+      setNowComponent(config.find((conf) => conf.name === value)!.component)
+    },
+    [ value ]
+  )
+
+  return (
+    <Block direction={Direction.COLUMN}>
+      {head}
+      <NowComponent />
+    </Block>
+  )
 }
 
 export default Sidebar
